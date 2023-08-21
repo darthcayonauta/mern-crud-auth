462d565bb2d8854c708bb94ed240e4eaa648bf0f
imatek23 <github@imatek.cl>
2023-08-21 16:50:57 -0400

    generando login y logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -23,9 +23,6 @@ export const register = async (req,res) => {
 
 
         res.cookie('token', token);
-        //res.json({
-        //     message: "User created successfully"
-        //})
 
         res.json({
             id: userSaved._id,
@@ -43,4 +40,51 @@ export const register = async (req,res) => {
     }
 } ;
 
-export const login = (req,res) => res.send('login') ;
+export const login = async (req,res) => {
+    //console.log(req.body);
+    const {email,password } = req.body;
+
+    try {
+
+        //buscamos usuario
+        const userFound = await User.findOne({email}) ;
+
+        if(!userFound) return res.status(400).json({message:"User not found"}) ;
+
+        //si todo esta ok, procedo a comparar las claves
+        const isMatch = await bcrypt.compare(password,userFound.password) ;
+
+        if(!isMatch) return res.status(400).json({message:"Incorrect password!"}) ;
+
+
+        const token = await createAccessToken({id:userFound._id}) ;
+
+
+        res.cookie('token', token);
+
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt,
+        }) ;
+
+
+        // console.log(newUser);
+    } catch (error) {
+        //console.log(error);
+        res.status(500).json({message:error.message}) ;
+    }
+} ;
+
+export const logout = (req,res) => {
+    res.cookie('token',"",{
+        expires: new Date(0) 
+    }) ;
+
+    //envio una respuesta al cliente
+    res.status(200) ;
+}
+
+//export const login = (req,res) => res.send('login') ;
