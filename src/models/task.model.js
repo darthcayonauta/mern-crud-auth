71b2ea7e95d003b7c3751d5c7817c96820d43b57
imatek23 <github@imatek.cl>
2023-08-22 15:36:03 -0400

    creacion de tasks

---
+++ src/models/task.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const taskSchema =  new mongoose.Schema({
+    title : {
+        type: String,
+        required : true,   
+    },
+    description :{
+        type : String,
+        required : true
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    }
+}, {timestamp: true})
+
+
+export default mongoose.model('Task', taskSchema) ;
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,11 +2,15 @@
 
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
+import { getTasks, getTask, createTask, updateTask, deleteTask  } from "../controllers/tasks.controller.js";
 
 
 const router = Router() ;
 
-router.get('/tasks',authRequired ,(req,res) => res.send('tareas')) ;
-
+router.get('/tasks',authRequired , getTasks) ;
+router.get('/tasks/:id',authRequired, getTask ) ;
+router.post('/tasks',authRequired, createTask ) ;
+router.put('/tasks',authRequired , updateTask) ;
+router.delete('/tasks',authRequired , deleteTask) ;
 
 export default router ;
