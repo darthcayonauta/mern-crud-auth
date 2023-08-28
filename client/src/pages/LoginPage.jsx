58c3e9aa0f205f8a335f6907d1106dd95e917fbf
imatek23 <github@imatek.cl>
2023-08-28 09:27:00 -0400

    final tasks

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,18 +1,24 @@
 import {useForm} from 'react-hook-form'
 import { useAuth } from '../context/AuthContext';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 
 function LoginPage() {
 
 //uso de useForm
 const {register,handleSubmit, formState: {errors}} = useForm() ;
-const {signin, errors:signinErrors} = useAuth()
+const {signin, errors:signinErrors, isAuthenticated} = useAuth()
+const navigate = useNavigate() ;
 
 const onSubmit = handleSubmit( data =>{ 
   //console.log(data);
   signin(data)
- } )
+ } ) ;
+
+useEffect(()=>{
+  if(isAuthenticated) navigate('/tasks')
+},[isAuthenticated]);
 
 
   return (
